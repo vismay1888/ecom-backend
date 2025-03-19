1c76df5e0b86e987b1e98b14cb8fb08d41ec19b1
vismay1888 <vismaysheelabhadra18@gmail.com>
2025-03-19 11:34:39 +0530

    final change

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const {User} = require('./model/User')
 const mongoose = require('mongoose');
 const cors =require('cors');
 const morgan =require('morgan');
-const bcrypt =require('bcrypt');
+const bcrypt =require('bcryptjs');
 const jwt =require('jsonwebtoken');
 const {Product}=require('./model/product');
 const {Cart}=require('./model/cart');
@@ -14,8 +14,12 @@ app.use(express.json());
 app.use(cors());
 app.use(morgan('dev'))
 
+//CcwipEstkVb3rxYK
 
-mongoose.connect('mongodb://127.0.0.1:27017/kleProject')
+let MONGODB_URL="mongodb+srv://vismaysheelabhadra18:CcwipEstkVb3rxYK@cluster0.d1er2.mongodb.net/?retryWrites=true&w=majority"
+
+
+mongoose.connect(MONGODB_URL)
 .then(()=>{
     console.log('db is connected')
 }).catch((err)=>{
